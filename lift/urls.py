f24ab5c96d457abb70a7cb368126f56031bf390f
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-12 01:08:52 +0500

    Register UI!

--- lift/urls.py
+++ lift/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.home_view, name="home")
+    path("", views.home_view, name="home"),
+    path("register", views.register_view, name="register"),
 ]
--- lift/views.py
+++ lift/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home_view(request):
     context = {}
+    return render(request, "lift/home.html", context)
+
+def register_view(request):
+    form = UserCreationForm()
+    context = {"form": form}
     return render(request, "lift/home.html", context)
