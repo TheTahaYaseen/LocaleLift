4c0437a8c273009769c964d298c118a035d3fb02
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-12 00:18:40 +0500

    Basic Pseudocode!

---
+++ pseudocode.py
@@ -0,0 +1,27 @@
+# Pseudocode For The Whole App
+
+# Key Features:
+
+"""
+
+Authentication:
+    # Standard Registration, Login And Logout
+    # Set their name, email and location as well.
+    Creation Of Business Profiles From User Profile Page.
+    All Business Profiles Associated Will Also Be Shown On User Profile Page.
+
+Business Profiles:
+    # Set their name, description, email, contact_number, category, owner, operating_hours, logo, cover_photo, website, social_medialinks and location.    
+    Able To Create, Update & Delete Business Profiles!
+    Able To Create, Update & Delete Promotions And Discounts!
+
+Promotions And Discounts:
+    # Set their business, title, description, discount_amount, start_date, end_date and target_audience.
+    Able To Create, Update & Delete Promotions And Discounts!
+
+Reviews And Ratings:
+    # Set their business, user, rating, and comment.
+    Able To Create, Update & Delete Review & Ratings!
+
+
+"""
